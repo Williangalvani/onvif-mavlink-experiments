758ed2ee03b47240f477bf81ca112c26bcccc9fa
Willian Galvani <williangalvani@gmail.com>
2021-06-16 21:14:18 -0300

    clean up

--- step1-discovery-into-dicts/discovery/src/main.rs
+++ step1-discovery-into-dicts/discovery/src/main.rs
@@ -3,11 +3,8 @@ use url::Url;
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
 
     use futures_util::stream::StreamExt;
-    const MAX_CONCURRENT_JUMPERS: usize = 100;
-    //let mut cameras = HashSet::new();
 
     let cameras_vec: Vec<onvif::discovery::Device>  = onvif::discovery::discover(std::time::Duration::from_secs(2))
         .await
